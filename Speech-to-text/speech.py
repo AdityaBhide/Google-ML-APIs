f5489cc0e41ec3e4936dd33fc65eb10ac7ce0223
Aditya Bhide <31139519+AdityaBhide@users.noreply.github.com>
2018-07-04 12:15:30 +0530

    Add files via upload

---
+++ Speech-to-text/speech.py
@@ -0,0 +1,32 @@
+import os
+import io
+
+def transcribe_file(speech_file):
+    """Transcribe the given audio file."""
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    client = speech.SpeechClient()
+
+    with io.open(speech_file , 'rb') as audio_file:
+        content = audio_file.read()
+        print(audio_file)
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code='en-US')
+
+    response = client.recognize(config, audio)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+
+    re=response.results[0].alternatives[0].transcript
+    print(re)
+    
+
+
+transcribe_file('chicago.flac')
